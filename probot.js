80557aba0b946063ab5cb642f355cf579564d332
fhmeghnnam <39240674+fhmeghnnam@users.noreply.github.com>
2018-12-09 22:32:35 +0200

    Update probot.js

--- probot.js
+++ probot.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const dateFormat = require('dateformat');
 const client = new Discord.Client(); 
 const Canvas = require("canvas"); //npm i canvas
-const prefix = "YOUR Prefix Here" // برفكس
+const prefix = "--" // برفكس
 let profile = JSON.parse(fs.readFileSync("profile.json", "utf8"))
 
 
@@ -824,4 +824,4 @@ message.channel.stopTyping()
 })
 }
 });
-client.login("token Your Bot Here");  // توكن حقق
+client.login("env.BOT_TOKEN");  // توكن حقق
