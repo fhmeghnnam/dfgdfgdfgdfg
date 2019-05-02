aabfa29d85a48955135e48d17db9cbb4a3c173a4
Fhme Ghnnam <39240674+fhmeghnnam@users.noreply.github.com>
2019-05-02 15:30:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,7 @@ client.on('guildMemberAdd', mem => {
 
 setTimeout(function () {
         
-if(mem.guild.channels.get('551057668585029632').send(" **_Welcome To _ Determind' Server **🥂 "));
+if(mem.guild.channels.get('572452695156457472').send(" **_Welcome To _ Determind' Server **🥂 "));
 }, 5000);
 })
 
