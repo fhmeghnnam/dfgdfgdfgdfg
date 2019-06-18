c1188cf57f0477ba93c5a4c7f66956f38b98bad9
Fhme Ghnnam <39240674+fhmeghnnam@users.noreply.github.com>
2019-06-18 17:44:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,21 +3,14 @@ const fs = require('fs'); // تعريف fs.
 const client = new Discord.Client(); // تعريف الكلينت
 const prefix = "$"; //تعريف البرفك
 
-client.on('guildMemberAdd', mem => {
-
-setTimeout(function () {
-        
-if(mem.guild.channels.get('572452695156457472').send(" **_Welcome To _ Ruby' Server **🥂 "));
-}, 5000);
-})
 
 
 client.on('ready',async () => {
 
 
 
-    client.channels.find(ch => ch.id === "551057699459170314" && ch.type === 'voice').join();
-    clie
+    client.channels.find(ch => ch.id === "582033515311529984" && ch.type === 'voice').join();
+    
   });
 	
 	
