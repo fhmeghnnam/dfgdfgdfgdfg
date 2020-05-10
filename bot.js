168ca61beee5985faf34dec73a8063a0efc485ba
Fhme Ghnnam <39240674+fhmeghnnam@users.noreply.github.com>
2020-05-11 02:08:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 let prefix = "!"
 client.on('ready',async () => {
-  client.channels.find(ch => ch.id === "704859219144212511" && ch.type === 'voice').join();
+  client.channels.find(ch => ch.id === "74859219144212511" && ch.type === 'voice').join();
 });
 
 
